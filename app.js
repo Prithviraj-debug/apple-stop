f5db8d6b491612fb55b01157c5090a895d4647e8
Prithviraj-debug <prithvirajmahamulkar100@gmail.com>
2021-07-10 22:31:18 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+// const iphone = document.querySelector('.iphone');
+
+// iphone.addEventListener('mousemove', (e) => {
+//     let xAxis = (window.innerWidth / 2 - e.pageX) / 30;
+//     let yAxis = (window.innerWidth / 2 - e.pageY) / 30;
+//     iphone.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`
+// });
+
+// container.addEventListener('mouseleave', e => {
+//     card.style.transition = 'all 0.5s ease';
+//     card.style.transform = `rotateY(0deg) rotateX(0deg)`
+// });
