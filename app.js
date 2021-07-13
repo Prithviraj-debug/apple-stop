65f4e1df544d10a562d2d9cca1bdb61106d932d1
ptIThvie <57887221+Prithviraj-debug@users.noreply.github.com>
2021-07-14 01:10:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,28 +1,15 @@
-// const iphone = document.querySelector('.iphone');
-
-// iphone.addEventListener('mousemove', (e) => {
-//     let xAxis = (window.innerWidth / 2 - e.pageX) / 30;
-//     let yAxis = (window.innerWidth / 2 - e.pageY) / 30;
-//     iphone.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`
-// });
-
-// container.addEventListener('mouseleave', e => {
-//     card.style.transition = 'all 0.5s ease';
-//     card.style.transform = `rotateY(0deg) rotateX(0deg)`
-// });
-
             document.querySelector('.hamburger').addEventListener('click', () => document.querySelector('.main-menu').classList.toggle('show'));
             document.querySelector('.hamburger').addEventListener('click', () => document.querySelector('body').classList.toggle('lock-scroll'));
             document.querySelector('.baag').addEventListener('click', () => document.querySelector('.main-menu').classList.toggle('showbag'));
 
       function grabbed(){
-        swal("Congrats!", ", Item added to your Bag!", "success");
+        swal("Congrats!", "Item added to your Bag!", "success");
       }
 
       function order(){
-        swal("Congrats!", ", Your order has been Placed!", "success");
+        swal("Order Placed!", "₹ 135,688 will be debited from your bank account!", "success");
       }
 
       function success(){
-        swal("Great!", ", Your Account has been Created Successfuly!", "success");
+        swal("Great!", "Your Account has been Created Successfuly!", "success");
       }
